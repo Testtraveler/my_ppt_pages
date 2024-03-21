e8d11ae451ce3963114df84830404e392ba666a5
“Testtraveler” <1943226078@qq.com>
2024-03-22 07:12:58 +0800

    my_ppt_NECCS

---
+++ assets/slidev/channel-d3ac35ac-D2EpKBUN.js
@@ -0,0 +1,1 @@
+import{aQ as o,aR as r}from"./7-Dfptvv-6.js";const s=(a,n)=>o.lang.round(r.parse(a)[n]),e=s;export{e as c};
---
+++ assets/slidev/print-Dw4NkWMB.js
@@ -0,0 +1,17 @@
+import{d as _,_ as u,y as h,b as a,e as t,x as s,A as r,F as f,Z as v,o as n,$ as g,l as x,g as b}from"../modules/vue-Z3Sauhpp.js";import{u as y,h as N,c as m,b as k}from"../index-8iCbUQNJ.js";import{N as w}from"./NoteDisplay-DsCzEyFo.js";import"../modules/shiki-Ba_aG2Bj.js";const A={id:"page-root"},D={class:"m-4"},L={class:"mb-10"},T={class:"text-4xl font-bold mt-2"},V={class:"opacity-50"},B={class:"text-lg"},C={class:"font-bold flex gap-2"},H={class:"opacity-50"},S=t("div",{class:"flex-auto"},null,-1),F={key:0,class:"border-main mb-8"},M=_({__name:"print",setup($){const{slides:d,total:p}=y();u(`
+@page {
+  size: A4;
+  margin-top: 1.5cm;
+  margin-bottom: 1cm;
+}
+* {
+  -webkit-print-color-adjust: exact;
+}
+html,
+html body,
+html #app,
+html #page-root {
+  height: auto;
+  overflow: auto !important;
+}
+`),N({title:`Notes - ${m.title}`});const i=h(()=>d.value.map(o=>{var l;return(l=o.meta)==null?void 0:l.slide}).filter(o=>o!==void 0&&o.noteHTML!==""));return(o,l)=>(n(),a("div",A,[t("div",D,[t("div",L,[t("h1",T,s(r(m).title),1),t("div",V,s(new Date().toLocaleString()),1)]),(n(!0),a(f,null,v(i.value,(e,c)=>(n(),a("div",{key:c,class:"flex flex-col gap-4 break-inside-avoid-page"},[t("div",null,[t("h2",B,[t("div",C,[t("div",H,s(e==null?void 0:e.no)+"/"+s(r(p)),1),g(" "+s(e==null?void 0:e.title)+" ",1),S])]),x(w,{"note-html":e.noteHTML,class:"max-w-full"},null,8,["note-html"])]),c<i.value.length-1?(n(),a("hr",F)):b("v-if",!0)]))),128))])]))}}),U=k(M,[["__file","C:/Users/Administrator/AppData/Roaming/npm/node_modules/@slidev/cli/node_modules/@slidev/client/pages/presenter/print.vue"]]);export{U as default};
